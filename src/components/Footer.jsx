6b751ef0a97a00aefb1955576ba458ad79ff528e
Srinadh1595 <srinadherakala1595@gmail.com>
2026-02-15 13:34:06 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,6 +32,7 @@ const Footer = () => {
             </div>
             <div className="footer-bottom">
                 <p>&copy; {new Date().getFullYear()} Sri Aadikonda Astrology. All Rights Reserved.</p>
+                <p>Developed by Srinadh</p>
             </div>
         </footer>
     );
